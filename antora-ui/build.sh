474f8094949326ecbd2e7ad7ca8c5e0e0d967bc6
alandefreitas <alandefreitas@gmail.com>
2023-06-08 22:00:09 -0300

    build(antora-ui): use local gulp installation
    fix #52

--- antora-ui/build.sh
+++ antora-ui/build.sh
@@ -2,22 +2,33 @@
 
 # This script is used to build the Antora-UI
 
+# Find npm
 npm_version=$(npm --version 2>/dev/null)
 if [ -z "$npm_version" ]; then
   echo "npm is not installed"
   exit 1
 fi
-# Add node_modules/.bin to path
-PATH=$(pwd)/node_modules/.bin:$PATH
-export PATH
-gulp_version=$(gulp --version 2>/dev/null)
-if [ -z "$gulp_version" ]; then
-  echo "gulp is not installed"
-  exit 1
-fi
 
+# Install modules if we have to
 if [ ! -d "node_modules" ] || [ "$(find package.json -prune -printf '%T@\n' | cut -d . -f 1)" -gt "$(find node_modules -prune -printf '%T@\n' | cut -d . -f 1)" ]; then
   npm ci
 fi
 
-gulp bundle
+# Find gulp
+PATH=$(pwd)/node_modules/.bin:$PATH
+export PATH
+gulp_version=$(gulp --version 2>/dev/null)
+if [ -n "$gulp_version" ]; then
+  GULP_CMD='gulp'
+else
+  npx_version=$(npx --version 2>/dev/null)
+  if [ -z "$npx_version" ]; then
+    echo "Neither gulp nor npx are installed"
+    exit 1
+  else
+    GULP_CMD='npx gulp'
+  fi
+fi
+
+
+$GULP_CMD bundle
