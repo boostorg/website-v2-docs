1ebdf6204d2f5f4b27c744f7dd48b801a6b71a72
sdarwin <samuel.d.darwin@gmail.com>
2023-11-06 21:23:26 +0000

    Run "npm ci"

--- libdoc.sh
+++ libdoc.sh
@@ -63,7 +63,7 @@ else
 fi
 
 if [ ! -d "node_modules" ] || [ "$(find package.json -prune -printf '%T@\n' | cut -d . -f 1)" -gt "$(find node_modules -prune -printf '%T@\n' | cut -d . -f 1)" ]; then
-  npm install
+  npm ci
 fi
 
 # Run antora command
