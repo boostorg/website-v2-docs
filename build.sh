b905d6471de4de2eb05c9274d42dba7f53f93d0b
alandefreitas <alandefreitas@gmail.com>
2023-11-17 14:20:53 -0300

    CI uses build script

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 if command -v git >/dev/null && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
   branch=$(git rev-parse --abbrev-ref HEAD)
+  echo "Setting branch to \"${branch}\""
 fi
 
 ./sitedoc.sh "$branch"
