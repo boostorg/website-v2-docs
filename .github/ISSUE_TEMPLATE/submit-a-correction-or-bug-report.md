49e844b332506bae563ce189f1dc1428a9390252
Mohammad Nejati <ashtumashtum@gmail.com>
2024-02-18 06:39:20 +0000

    Add license header to all source files

--- .github/ISSUE_TEMPLATE/submit-a-correction-or-bug-report.md
+++ .github/ISSUE_TEMPLATE/submit-a-correction-or-bug-report.md
@@ -1,3 +1,11 @@
+<!--
+Copyright (c) 2024 The C++ Alliance, Inc. (https://cppalliance.org)
+
+Distributed under the Boost Software License, Version 1.0. (See accompanying
+file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+Official repository: https://github.com/boostorg/website-v2-docs
+-->
 ---
 name: Submit a Correction or Bug report
 about: This is for typos, grammar, formatting, UI, HTML, or CSS style problems.
--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+#
+# Copyright (c) 2024 The C++ Alliance, Inc. (https://cppalliance.org)
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
+# Official repository: https://github.com/boostorg/website-v2-docs
+#
 
 if command -v git >/dev/null && git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
   branch=$(git rev-parse --abbrev-ref HEAD)
